f6b9e4b3b0ba84329b59196cdd446becb11f26bb
MOHITGUPTA6510 <mohitguptakvs@gmail.com>
2026-08-13 00:51:17 +0530

    takeing input

---
+++ DSA/TakeInput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class TakeInput{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //nextInt() , nextFloat()
+        System.out.println(name);
+    }
+}
